3e4a482b19cdabe30d32e5eabacc02e677ae6d06
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-22 11:33:54 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Login from './components/Login';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [user, setUser] = React.useState('tatiana');
+    
+    React.useEffect(() => {
+        document.title = user ? `${user}'s Feed` : 'Please login';
+    }, [user])
+
+    if (!user) {
+        return <Login setUser={setUser}/>
+
+    }
+
+    return <>
+        <Header user={user} setUser={setUser} />
+    </>
 }
 
 export default App;
