3c3c4dd753f05d6569baa367834d053b1fbc82d3
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-23 10:16:03 -0500

    useReducer hook added

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,14 @@
 import React, { useContext } from 'react';
-import { UserContext } from '../App';
+import { UserContext, PostContext } from '../App';
 
-function Post({ image, content, user }) {
+function Post({ image, content, user, id }) {
     const currentUser = React.useContext(UserContext);
+    const { dispatch } = React.useContext(PostContext);
     const isCurrentUser = currentUser === user;
+
+    function handleDeletePost() {
+        dispatch({ type: "DELETE_POST", payload: { id }});
+    }
 
     return (
         <>
@@ -15,7 +20,8 @@ function Post({ image, content, user }) {
                 />
             )}
             <p>{content}</p>
-            <div style={{ color: isCurrentUser && "green" }}>{user}</div>
+            <div style={{ color: isCurrentUser && "orange" }}>{user}</div>
+            { isCurrentUser && <button onClick={handleDeletePost}>Delete</button> }
         </>
     )
 }
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,7 +3,7 @@ import Post from './Post';
 
 function PostList({ posts }) {
     return posts.map((post, i) => (
-        <Post key={i} {...post} />
+        <Post key={post.id} {...post} />
     ))
 }
 
